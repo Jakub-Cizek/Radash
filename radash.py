9c56bef200fbfeec9e4dc7b6a1afc2f696109b16
Jakub-Cizek <jakub.cizek@mff.cuni.cz>
2024-12-03 12:37:39 +0100

    Add files via upload

--- radash.py
+++ radash.py
@@ -9,7 +9,9 @@ class Material:
         self.number_composition_element=np.size(composition_element)
         self.composition_Z=np.zeros(self.number_composition_element,dtype=int)
         self.composition_units='a'
-        
+        self.Ed=0
+
+        iserr=0
         #renormalization of concentrations
         sum=0
         for i in range(self.number_composition_element):
@@ -243,14 +245,15 @@ class Material:
             self.composition_Z[i]=112     
         for i in range(self.number_composition_element):
           if self.composition_Z[i]==0:
+            iserr=1  
             print(f"error: unknown element symbol {self.composition_element[i]}")                    
-        
-        print(f"material: {self.name}")
-        print(f"number of constituents: {self.number_composition_element}")
-        for i in range(self.number_composition_element):
-          print(f"{self.composition_element[i]:2s} {self.composition_Z[i]:2d} {self.composition_concentration[i]:6.3f}")
-        if self.composition_units=='a':
-          print(f"units: atomic")
+        if iserr==0:
+            print(f"material: {self.name}")
+            print(f"number of constituents: {self.number_composition_element}")
+            for i in range(self.number_composition_element):
+              print(f"{self.composition_element[i]:2s} {self.composition_Z[i]:2d} {self.composition_concentration[i]:6.3f}")
+            if self.composition_units=='atomic':
+              print(f"units: atomic")
         
     def __str__(self):
         return f"material: {self.name}"    
@@ -261,6 +264,24 @@ class Material:
         for i in range(self.number_composition_element):
           self.Zeff=self.Zeff+self.composition_Z[i]*self.composition_concentration[i]
         print(f"effective Z number: {self.Zeff:5.3f}")
+
+    #average molar weight     
+    def get_molar_weight(self):
+         data_molar_weight=np.loadtxt("molar_weight.txt")
+         number_data_molar_weight=np.size(data_molar_weight[:,0])
+         self.composition_molar_weight=np.zeros(self.number_composition_element)
+         for i in range(self.number_composition_element):
+          for j in range(number_data_molar_weight):
+            if self.composition_Z[i]==data_molar_weight[j,0]:
+              self.composition_molar_weight[i]=data_molar_weight[j,1]
+         for i in range(self.number_composition_element):
+          if self.composition_molar_weight[i]==0:
+            print(f"error: no data for molar weight of element {self.composition_element[i]}")     
+         self.molar_weight=0     
+         for i in range(self.number_composition_element):
+          print(f"{self.composition_element[i]:2s} {self.composition_molar_weight[i]:7.3f} g/mol")
+          self.molar_weight=self.molar_weight+self.composition_molar_weight[i]*self.composition_concentration[i]
+         print(f"composition weighted molar weight: {self.molar_weight:7.3f} g/mol")
         
     #average density    
     def get_density(self):
@@ -348,3 +369,205 @@ class Material:
           print(f"{self.composition_element[i]:2s} {self.composition_neutron_cross_section[i]:6.3f} barn")
           self.neutron_cross_section=self.neutron_cross_section+self.composition_neutron_cross_section[i]*self.composition_concentration[i]
          print(f"composition weighted neutron cross-section: {self.neutron_cross_section:5.3f} barn")     
+
+    #treshold atom displacement energy Ed
+    def get_Ed(self):
+         print(f"determination of threshold atom displacement energy")
+         data_Ed=np.loadtxt("Ed.txt")
+         number_data_Ed=np.size(data_Ed[:,0])
+         self.composition_Ed=np.zeros(self.number_composition_element)
+         for i in range(self.number_composition_element):
+          for j in range(number_data_Ed):
+            if self.composition_Z[i]==data_Ed[j,0]:
+              self.composition_Ed[i]=data_Ed[j,1]
+         for i in range(self.number_composition_element):
+          if self.composition_Ed[i]==0:
+            print(f"error: no data for threshold atom displacement energy of element {self.composition_element[i]}")     
+         self.Ed=0     
+         for i in range(self.number_composition_element):
+          print(f"{self.composition_element[i]:2s} {self.composition_Ed[i]:5.3f} eV")
+          self.Ed=self.Ed+self.composition_Ed[i]*self.composition_concentration[i]
+         print(f"composition weighted threshold atom displacement energy: {self.Ed:5.3f} eV")
+    
+    #neutron irradiation NRT dpa
+    def get_NRT_dpa(self,energy,fluence,thickness,**kwargs):
+        self.get_density()
+        print("----------------------------------------------------------------------------------------------------")
+        self.get_molar_weight()
+        print("----------------------------------------------------------------------------------------------------")
+        self.get_Ed()
+        Na=6.02214076e23 #Avogadro number (1/mol) 
+        #neutron Fluence (1/cm2)
+        #neutron Energy (MeV)
+        #sample thickness (mm)
+        #molar weight (g/mol)
+        if energy*1e6<self.Ed:
+            self.NRT_dpa=0
+        elif energy*1e6>=self.Ed and energy*1e6<2*self.Ed/0.8:
+            self.NRT_dpa = fluence*1e4/(thickness*1e-3*self.density*1e3*Na/(self.molar_weight*1e-3))
+        elif energy*1e6>=2*self.Ed/0.8:   
+            self.NRT_dpa = 0.8*energy*1e6/(2*self.Ed)*fluence*1e4/(thickness*1e-3*self.density*1e3*Na/(self.molar_weight*1e-3))
+        print("----------------------------------------------------------------------------------------------------")
+        print(f"{energy:7.3f} MeV neutrons, fluence: {fluence/1e16:5.3f} x 1e16 1/cm2,  NRT dpa: {self.NRT_dpa:5.3f}")  
+        print("----------------------------------------------------------------------------------------------------")
+        
+
+    def get_arc_dpa_constants(self,**kwargs):
+         print(f"arc dpa constants")
+         data_arc_dpa_constants=np.loadtxt("arc_dpa_constants.txt")
+         number_data_arc_dpa_constants=np.size(data_arc_dpa_constants[:,0])
+         self.composition_arc_dpa_constants=np.zeros((self.number_composition_element,2))
+         for i in range(self.number_composition_element):
+          for j in range(number_data_arc_dpa_constants):
+            if self.composition_Z[i]==data_arc_dpa_constants[j,0]:
+              self.composition_arc_dpa_constants[i,0]=data_arc_dpa_constants[j,1]
+              self.composition_arc_dpa_constants[i,1]=data_arc_dpa_constants[j,2]
+         for i in range(self.number_composition_element):
+          if self.composition_arc_dpa_constants[i,0]==0:
+            print(f"error: no data for arc dpa constants of element {self.composition_element[i]}")     
+         self.arc_dpa_constants=np.zeros(2)     
+         for i in range(self.number_composition_element):
+          print(f"{self.composition_element[i]:2s} b = {self.composition_arc_dpa_constants[i,0]:5.3f}, c = {self.composition_arc_dpa_constants[i,1]:5.3f}")
+          self.arc_dpa_constants[0]=self.arc_dpa_constants[0]+self.composition_arc_dpa_constants[i,0]*self.composition_concentration[i]
+          self.arc_dpa_constants[1]=self.arc_dpa_constants[1]+self.composition_arc_dpa_constants[i,1]*self.composition_concentration[i]
+         print(f"composition weighted arc dpa constants: b = {self.arc_dpa_constants[0]:5.3f}, c = {self.arc_dpa_constants[1]:5.3f}")
+         iplot=False 
+         if kwargs.get('plot', iplot) == True:
+             energy_plot=np.linspace(0.1,100,10000)
+             plt.plot(energy_plot,self.arc_dpa_efficiency_function(energy_plot*1e6,self.Ed,self.arc_dpa_constants[0],self.arc_dpa_constants[1]))
+             plt.xscale('log')
+             plt.xlabel('energy (MeV)')
+             plt.ylabel('arc dpa efficiency')
+             plt.title('arc dpa efficiency function')
+             plt.show()
+
+    def arc_dpa_efficiency_function(self,energy,Ed,b,c):
+         return (1-c)/(2*Ed/0.8)**b*energy**b+c
+        
+      
+    #neutron irradiation arc dpa
+    def get_arc_dpa(self,energy,fluence,thickness,**kwargs):
+        self.get_density()
+        print("----------------------------------------------------------------------------------------------------")
+        self.get_molar_weight()
+        print("----------------------------------------------------------------------------------------------------")
+        self.get_Ed()
+        print("----------------------------------------------------------------------------------------------------")
+        self.get_arc_dpa_constants()
+        Na=6.02214076e23 #Avogadro number (1/mol) 
+        #neutron Fluence (1/cm2)
+        #neutron Energy (MeV)
+        #sample thickness (mm)
+        #molar weight (g/mol)
+        if energy*1e6<self.Ed:
+            self.arc_dpa=0
+        elif energy*1e6>=self.Ed and energy*1e6<2*self.Ed/0.8:
+            self.arc_dpa = fluence*1e4/(thickness*1e-3*self.density*1e3*Na/(self.molar_weight*1e-3))
+        elif energy*1e6>=2*self.Ed/0.8:
+            xi=self.arc_dpa_efficiency_function(energy*1e6,self.Ed,self.arc_dpa_constants[0],self.arc_dpa_constants[1])
+            self.arc_dpa = 0.8*energy*1e6/(2*self.Ed)*fluence*1e4/(thickness*1e-3*self.density*1e3*Na/(self.molar_weight*1e-3))*xi
+        print("----------------------------------------------------------------------------------------------------")
+        print(f"{energy:7.3f} MeV neutrons, fluence: {fluence/1e16:5.3f} x 1e16 1/cm2,  arc dpa: {self.arc_dpa:5.3f}")
+        print("----------------------------------------------------------------------------------------------------")    
+            
+
+    #neutron irradiation NRT dpa for spectrum of neutron energies
+    def get_NRT_dpa_spectrum(self,fluence,thickness,**kwargs):
+        self.get_density()
+        print("----------------------------------------------------------------------------------------------------")
+        self.get_molar_weight()
+        print("----------------------------------------------------------------------------------------------------")
+        self.get_Ed()
+        print("----------------------------------------------------------------------------------------------------")
+        Na=6.02214076e23 #Avogadro number (1/mol) 
+        #neutron Fluence (1/cm2)
+        #neutron Energy (MeV)
+        #sample thickness (mm)
+        #molar weight (g/mol)
+        spectrum_file_name=''
+        spectrum_file_name=kwargs.get('spectrum',spectrum_file_name)
+        print(f"spectrum file: {spectrum_file_name}")
+        data=np.loadtxt(spectrum_file_name)
+        E=data[:,0]
+        I=data[:,1]
+        n_E=np.size(E)
+        delta_E=np.zeros(n_E)
+        E_old=E[0]
+        for i in range(n_E):
+            delta_E[i]=E[i]-E_old
+            E_old=E[i]
+        I_sum=0
+        for i in range(n_E):
+            I_sum+=I[i]*delta_E[i]*1e6
+        I_norm=I/I_sum
+        iplot=False
+        if kwargs.get('plot',iplot)==True:
+            plt.plot(E,I_norm)
+            plt.xlabel('energy (MeV)')
+            plt.ylabel('relative fraction')
+            plt.xscale('log')
+            plt.yscale('log')
+            plt.show()    
+        
+        self.NRT_dpa=0
+        for i in range(n_E): 
+            if E[i]*1e6<self.Ed:
+                self.NRT_dpa += 0
+            elif E[i]*1e6>=self.Ed and E[i]*1e6<2*self.Ed/0.8:
+                self.NRT_dpa += fluence*1e4/(thickness*1e-3*self.density*1e3*Na/(self.molar_weight*1e-3))*delta_E[i]*1e6
+            elif E[i]*1e6>=2*self.Ed/0.8:   
+                self.NRT_dpa += 0.8*E[i]*1e6/(2*self.Ed)*fluence*I_norm[i]*1e4/(thickness*1e-3*self.density*1e3*Na/(self.molar_weight*1e-3))*delta_E[i]*1e6
+        print(f"fluence: {fluence/1e16:5.3f} x 1e16 1/cm2,  NRT dpa: {self.NRT_dpa:5.3f}")   
+
+    #neutron irradiation arc dpa for spectrum of neutron energies
+    def get_arc_dpa_spectrum(self,fluence,thickness,**kwargs):
+        self.get_density()
+        print("----------------------------------------------------------------------------------------------------")
+        self.get_molar_weight()
+        print("----------------------------------------------------------------------------------------------------")
+        self.get_Ed()
+        print("----------------------------------------------------------------------------------------------------")
+        self.get_arc_dpa_constants()
+        print("----------------------------------------------------------------------------------------------------")
+        Na=6.02214076e23 #Avogadro number (1/mol) 
+        #neutron Fluence (1/cm2)
+        #neutron Energy (MeV)
+        #sample thickness (mm)
+        #molar weight (g/mol)
+        spectrum_file_name=''
+        spectrum_file_name=kwargs.get('spectrum',spectrum_file_name)
+        data=np.loadtxt(spectrum_file_name)
+        print(f"spectrum file: {spectrum_file_name}")
+        E=data[:,0]
+        I=data[:,1]
+        n_E=np.size(E)
+        delta_E=np.zeros(n_E)
+        E_old=E[0]
+        for i in range(n_E):
+            delta_E[i]=E[i]-E_old
+            E_old=E[i]
+        I_sum=0
+        for i in range(n_E):
+            I_sum += I[i]*delta_E[i]*1e6
+        I_norm=I/I_sum
+        self.arc_dpa=0
+        iplot=False
+        if kwargs.get('plot',iplot)==True:
+            plt.plot(E,I_norm)
+            plt.xlabel('energy (MeV)')
+            plt.ylabel('relative fraction')
+            plt.xscale('log')
+            plt.yscale('log')
+            plt.show()
+        for i in range(n_E):
+            if E[i]*1e6<self.Ed:
+                self.arc_dpa += 0
+            elif E[i]*1e6>=self.Ed and E[i]*1e6<2*self.Ed/0.8:
+                self.arc_dpa += fluence*1e4/(thickness*1e-3*self.density*1e3*Na/(self.molar_weight*1e-3))*delta_E[i]*1e6
+            elif E[i]*1e6>=2*self.Ed/0.8:
+                #xi=(1-self.arc_dpa_constants[1])/(2*self.Ed/0.8)**self.arc_dpa_constants[0]*energy*1e6**self.arc_dpa_constants[0]+self.arc_dpa_constants[1]
+                xi=self.arc_dpa_efficiency_function(E[i]*1e6,self.Ed,self.arc_dpa_constants[0],self.arc_dpa_constants[1])
+                self.arc_dpa += 0.8*E[i]*1e6/(2*self.Ed)*fluence*I_norm[i]*1e4/(thickness*1e-3*self.density*1e3*Na/(self.molar_weight*1e-3))*xi*delta_E[i]*1e6
+        print(f"fluence: {fluence/1e16:5.3f} x 1e16 1/cm2,  arc dpa: {self.arc_dpa:5.3f}")
+            
+    
